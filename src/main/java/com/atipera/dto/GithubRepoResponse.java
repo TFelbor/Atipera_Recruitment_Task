7abe13e8a630640ce34c9ec1d217ce46e24a7383
Tytus Felbor <146979682+TFelbor@users.noreply.github.com>
2025-08-04 20:43:26 +0200

    Create GithubRepoResponse.java

---
+++ src/main/java/com/atipera/dto/GithubRepoResponse.java
@@ -0,0 +1,43 @@
+package com.atipera.dto;
+
+public class GithubRepoResponse {
+    private String name;
+    private Owner owner;
+    private boolean fork;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public boolean isFork() {
+        return fork;
+    }
+
+    public void setFork(boolean fork) {
+        this.fork = fork;
+    }
+
+    public static class Owner {
+        private String login;
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+    }
+}
